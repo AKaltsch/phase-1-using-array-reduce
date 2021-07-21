32b2eb8f32d6df7e0ff939769b975811c6c3795e
alec <kaltschmidalec@gmail.com>
2021-07-21 15:49:40 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const total = (accumulator, item) => {
+    return accumulator += item
+}
+
+let totalBatteries = batteryBatches.reduce(total, 0)
